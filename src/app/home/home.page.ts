bae122f62d3d26571c655b7232e3f7c20548e30e
Maro de Melo <marodemelo10@gmail.com>
2021-10-15 09:21:46 -0300

    salvando as tasks no localStorage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,10 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private alertCtrl: AlertController) { }
+
+  tasks: any[] = []
+
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController) { }
 
 
   async showAdd() {
@@ -16,7 +19,7 @@ export class HomePage {
       header: "O que deseja fazer?",
       inputs: [
         {
-          name: 'task',
+          name: 'newTask',
           type: 'text',
           placeholder: 'O que deseja fazer'
 
@@ -33,8 +36,8 @@ export class HomePage {
         },
         {
           text: 'Adicionar',
-          handler: (form)=> {
-            //this.add(form.task)
+          handler: (form) => {
+            this.add(form.newTask)
             console.log(form)
           }
         }
@@ -42,5 +45,25 @@ export class HomePage {
       ]
     })
     await alert.present()
+  }
+
+  async add(newTask: string) {
+    //valida se usuário preencheu a task
+    if (newTask.trim().length < 1) {
+      const toast = await this.toastCtrl.create({
+        message: 'Informe o que deseja fazer!',
+        duration: 2000,
+        position: 'top'
+      })
+      toast.present()
+      return
+    }
+    let task = {name: newTask, done: false}
+
+    this.tasks.push(task)
+    this.updateLocalStorage()
+  }
+  updateLocalStorage(){
+    localStorage.setItem('taskDb', JSON.stringify(this.tasks))
   }
 }
