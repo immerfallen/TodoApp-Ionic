410cc798de987874cc068b360d67d026d066d1bd
Maro de Melo <marodemelo10@gmail.com>
2021-10-15 08:57:42 -0300

    criado alert para inserir a task

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertCtrl: AlertController) { }
 
+
+  async showAdd() {
+    const alert = await this.alertCtrl.create({
+      header: "O que deseja fazer?",
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'O que deseja fazer'
+
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'danger',
+          handler: () => {
+            console.log('clicked cancel')
+          }
+        },
+        {
+          text: 'Adicionar',
+          handler: (form)=> {
+            //this.add(form.task)
+            console.log(form)
+          }
+        }
+
+      ]
+    })
+    await alert.present()
+  }
 }
